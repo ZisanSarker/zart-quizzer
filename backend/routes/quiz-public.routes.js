55066b47c5104624887144553af24bd8b7a7af77
ZisanSarker <sakibuzzamanjisan@gmail.com>
2025-08-05 22:45:46 +0600

    feat(ui): added several pages to home page

--- backend/routes/quiz-public.routes.js
+++ backend/routes/quiz-public.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   getPublicQuizzes,
   getQuizById,
-  getPopularSearchQueries
+  getPopularSearchQueries,
+  getTrendingQuizzes
 } = require('../controllers/quiz.controller');
 
 router.get('/explore', getPublicQuizzes);
+router.get('/trending', getTrendingQuizzes);
 router.get('/popular-search-queries', getPopularSearchQueries);
 router.get('/:id', getQuizById);
 
